56a539543556bd45cee2ba0ce7175678959b9fc8
itstruce <truce.xyz@gmail.com>
2024-11-26 18:59:41 +0530

    remove solve() function from intercepted_inputs.cpp

--- intercepted_inputs.cpp
+++ intercepted_inputs.cpp
@@ -1,45 +1,39 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-    int k;
-    cin >> k;
-    
-    map<int, int> m;
-
-    for (int i = 0; i < k; i++)
-    {
-        int a;
-        cin >> a;
-        
-        m[a]++;
-    }
-    
-    int req = k - 2;
-
-    for (int i = 1; i * i <= req; i++)
-    {
-        int q = req / i;
-        
-        if (q * i == req && m.find(i) != m.end() && m.find(q) != m.end())
-        {
-            cout << i << " " << q << "\n";
-            return;
-        }
-    }
-}
-
-int intercepted_inputs()
+int main()
 {
     int t;
     cin >> t;
-    vector<vector<int>> inputs;
-    
-    for (int i = 0; i < t; ++i)
+
+    while (t--)
     {
-        solve();
+        int k;
+        cin >> k;
+
+        map<int, int> m;
+
+        for (int i = 0; i < k; i++)
+        {
+            int a;
+            cin >> a;
+
+            m[a]++;
+        }
+
+        int req = k - 2;
+
+        for (int i = 1; i * i <= req; i++)
+        {
+            int q = req / i;
+
+            if (q * i == req && m.find(i) != m.end() && m.find(q) != m.end())
+            {
+                cout << i << " " << q << "\n";
+                break;
+            }
+        }
     }
-    
+
     return 0;
 }
