2f2b34268c45f5c1de8d25817c3b8747ab3663ec
itstruce <truce.xyz@gmail.com>
2024-12-22 13:13:59 +0530

    more codechef

---
+++ reverse_punctuation.cpp
@@ -0,0 +1,33 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int reverse_punctuation()
+{
+    int n;
+    cin >> n;
+    
+    vector<string> v;
+    string in;
+    
+    while (cin >> in)
+        v.push_back(in);
+    
+    reverse(v.begin(), v.end());
+    
+    for (int i = 0; i < v.size(); ++i)
+    {
+        auto s = v[i];
+        
+        for (auto c : s)
+        {
+            if (c == '\'' || c == '.' || c == ',' || c == ';' || c == ':')
+                continue;
+            else
+                cout << c;
+        }
+        
+        cout << (i > 0 && s.find('.') != variant_npos ? "\n" : " ");
+    }
+    
+    return 0;
+}
---
+++ voters_list.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int voters_list()
+{
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    
+    int total = n1 + n2 + n3;
+    
+    map<int, int> m;
+    set<int> final;
+    
+    while (total--)
+    {
+        int a;
+        cin >> a;
+        
+        m[a]++;
+    }
+    
+    for (auto x : m)
+    {
+        if (x.second > 1)
+            final.insert(x.first);
+    }
+    
+    cout << final.size() << "\n";
+    for (auto x : final)
+    {
+        cout << x << "\n";
+    }
+    
+    return 0;
+}
