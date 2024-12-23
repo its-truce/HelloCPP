d325afb36f6cbc6d110b191a96026bd2a9023f62
itstruce <truce.xyz@gmail.com>
2024-12-23 14:23:30 +0530

    round 995 (haven't submitted yet)

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -1,3 +1,26 @@
-﻿//
-// Created by Lenovo on 23-12-2024.
-//
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int next_permutation()
+{
+    int n, k;
+    cin >> n >> k;
+    
+    while (k--)
+    {
+        vector<int> permutation(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> permutation[i];
+        }
+        
+        next_permutation(permutation.begin(), permutation.end());
+        for (auto x : permutation)
+        {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
---
+++ prep_for_the_olympiad.cpp
@@ -0,0 +1,36 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int prep_for_the_olympiad()
+{
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        
+        vector<int> m(n), s(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> m[i];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> s[i];
+        }
+
+        int diff = 0;
+        for (int i = 0; i < n-1; ++i)
+        {
+            if (m[i] > s[i+1])
+                diff += (m[i] - s[i+1]);
+        }
+        diff += m[n-1];
+        
+        cout << diff << "\n";
+    }
+    
+    return 0;
+}
