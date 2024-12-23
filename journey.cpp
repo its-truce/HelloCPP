a19770755cbfd6345a374e38648947e162bb368d
itstruce <truce.xyz@gmail.com>
2024-12-23 14:34:49 +0530

    round 995 div. 3

---
+++ journey.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        int n, v[3];
+        cin >> n >> v[0] >> v[1] >> v[2];
+
+        int total = v[0] + v[1] + v[2];
+        int cycles = n / total; 
+        int r = n % total; 
+
+        int day = cycles * 3;
+        for (int i = 0; i < 3 && r > 0; ++i)
+        {
+            r -= v[i];
+            day++;
+        }
+        
+        cout << day << "\n";
+    }
+    
+    return 0;
+}
