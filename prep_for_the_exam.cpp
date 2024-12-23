295acc92e5278effa0dd5e1eaf0d3b3bbbda7d71
itstruce <truce.xyz@gmail.com>
2024-12-23 15:18:20 +0530

    round 995 div. 3 prob C done

---
+++ prep_for_the_exam.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        int n, m, k;
+        string out;
+        cin >> n >> m >> k;
+        
+        int lists[m];
+        bitset<30001> known;
+        
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> lists[i];
+        }
+        for (int i = 0; i < k; ++i)
+        {
+            int a;
+            cin >> a;
+            known[a] = true;
+        }
+        
+        for (auto x : lists)
+        {
+            if (k != n - 1)
+            {
+                out.push_back((k < n - 1) ? '0' : '1');
+                continue;
+            }
+
+            out.push_back(known[x] ? '0' : '1');
+        }
+        
+        cout << out << "\n";
+    }
+    
+    return 0;
+}
